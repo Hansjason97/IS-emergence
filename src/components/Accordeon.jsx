fc07be0f40a2f979e8ba0157b10221a6e30b6ff8
Hansjason97 <hansadiogo@gmail.com>
2023-06-15 16:26:35 +0100

    fixed accordeon

--- src/components/Accordeon.jsx
+++ src/components/Accordeon.jsx
@@ -8,8 +8,8 @@ function Accordeon(props) {
     }
 
   return (
-    <div className='max-w-[350px] w-full'>
-        <p className='px-[15px] py-[5px] flex items-center justify-between capitalize border-b border-primary text-base cursor-pointer' onClick={toggle}>
+    <div className='max-w-[350px] w-full' onMouseEnter={toggle} onMouseLeave={toggle}>
+        <p className='px-[15px] py-[5px] flex items-center justify-between capitalize border-b border-primary text-base cursor-pointer'>
             {props.title}
             { open === false ? (<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 ">
   <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
